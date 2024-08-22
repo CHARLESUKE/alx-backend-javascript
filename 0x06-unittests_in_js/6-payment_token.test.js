647d6e6ba7165c5225968985dfa0853297aaa3e3
CHARLESUKE <ukcharleay@gmail.com>
2024-08-22 22:00:31 +0000

    0x06-unittests_in_js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -10,16 +10,4 @@ describe("getPaymentTokenFromAPI", function() {
 		done();
 	    });
     });
-});const getPaymentTokenFromAPI = require("./6-payment_token");
-const {describe, it} = require("mocha");
-const expect = require("chai").expect;
-
-describe("getPaymentTokenFromAPI", function() {
-    it("Async testing with done callback", function(done) {
-	getPaymentTokenFromAPI(true)
-	    .then((data) => {
-		expect(data).to.have.property('data');
-		done();
-	    });
-    });
 });
